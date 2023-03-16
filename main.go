37debc50af566bda6290fc75be9b93b3b7cbd248
Kesha Dave <kesha.11042@gmail.com>
2023-03-16 13:34:35 -0400

    First Commit
    Here we are performing Concurrent Programming using GOROUTINE.

---
+++ main.go
@@ -0,0 +1,19 @@
+// concurrent programming using GOROUTINE
+package main
+
+import (
+	"fmt"
+)
+
+// create a function
+func display(message string) {
+	fmt.Println(message)
+}
+
+func main() {
+
+	// call goroutine
+	go display("Process 1")
+
+	display("Process 2")
+}
